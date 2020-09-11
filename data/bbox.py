4516d61092435e41d937eb01e9fc2c2335e794b0
Aryan Sakaria <AryanSakaria@github.com>
2020-09-11 21:14:47 +0300

    added bbox generation

--- data/bbox.py
+++ data/bbox.py
@@ -1,7 +1,11 @@
 import numpy as np 
 from PIL import Image 
+import sys
+sys.path.remove(sys.path[1])
 import cv2
-import matplotlib.pyplot as plt 
+# from tqdm import tqdm
+import os
+# import matplotlib.pyplot as plt 
 
 
 def image_bbox(image_path):
@@ -22,5 +26,40 @@ def image_bbox(image_path):
 			for c in contours:
 				x, y, w, h = cv2.boundingRect(c)
 				contour_stack.append([i, x, x + w, y, y + h])
+	contour_stack = np.array(contour_stack, dtype = np.uint16)
 	return contour_stack
-				
+
+
+if __name__ == '__main__':
+	#generate for all images.
+	data_path = "/media/aryan/DATA/Dataset_pedestrian/Small_Obstacle_Dataset"
+	# img_list = []
+	# save_list = []
+	for split in os.listdir(data_path):
+		split_path = os.path.join(data_path, split)
+		# print(split_path)
+		for seq in os.listdir(split_path):
+			seq_path = os.path.join(split_path, seq)
+			labels_path = os.path.join(seq_path, "labels")
+			bbox = os.path.join(seq_path, "bbox")
+
+			if not os.path.isdir(bbox):
+				os.makedirs(bbox)
+			# print(os.listdir(seq_path))
+			labels = os.listdir(labels_path)
+			for label in labels:
+				img_path = os.path.join(labels_path, label)
+				save_path = label[:-4]
+				save_path = os.path.join(bbox, save_path + ".npy")
+				# img_list.append(img_path)
+				# save_list.append(save_path)
+				bbox_data = image_bbox(img_path)
+				np.save(save_path, bbox_data)
+				print(save_path)
+				# print(bbox_data)
+				# print(save_path)
+				# print(img_path)
+				# np.save(save_path, bbox_data)
+				# print(img_path)
+
+
